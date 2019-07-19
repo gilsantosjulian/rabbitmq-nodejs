b7465fa6861144c2826ad97ecbd17ef8d3fb313d
Julian <julian@minka.io>
2019-07-19 18:01:12 -0500

    feat(send.js) confihure camelcase headers

--- send.js
+++ send.js
@@ -1,6 +1,5 @@
 const amqp = require('amqplib/callback_api');
 
-const channel = null;
 const queueName = 'julian-queue';
 const exchange = 'julian-exchange';
 const routingKey = 'julian-routing-key';
@@ -14,55 +13,51 @@ const rabbitmqSettings = {
   password: 'Qq4FaMjW',
   vhost: '/',
   authMechanism: ['PLAIN', 'AMQPLAIN', 'EXTERNAL'],
-  payload: ''
 };
 
-const initConnection = () => {
-  let newSettings = {
-    ...rabbitmqSettings,
-    payload: payload
+amqp.connect(rabbitmqSettings, (error, connection) => {
+  if (error) {
+    throw error;
   }
-  return amqp.connect(newSettings, (error, connection) => {
+  connection.createChannel((error, channel) => {
     if (error) {
       throw error;
     }
-    connection.createChannel((error, channel) => {
-      if (error) {
-        throw error;
-      }
 
-      channel.assertQueue(queueName, {
-        durable: true
-      });
+    channel.assertQueue(queueName, {
+      durable: true
     });
+
+    let payload = {
+      key1: 'Julian',
+      key2: 'Julian'
+    };
+
+    let headers = {
+      JMSMessageID: 1324,
+      JMSPriority: 'Normal',
+      JMSTimeStamp: 231231,
+      JMSCorrelationID: 1264,
+    }
+
+    let data = {
+      expiration: 12653412,
+      delivery_mode: 2,
+      headers: headers,
+    }
+
+    channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(payload)), data);
+    console.log(" [x] Sent %s", payload);
   });
-}
-
-export const publish = async () => {
-  initConnection();
-  let payload = {
-    key1: 'Julian',
-    key2: 'Julian'
-  };
-
-  let headers = {
-    'JMS-Message-Priority': 'Normal',
-    'JMS-Message-Time-Stamp': timestamp,
-    'JMS-Message-Type': 'Test Message',
-    'JMS-Message-ID': 1324,
-  }
-
-  Buffer(JSON.stringify(payload)), {
-    expiration: timestamp,
-    'delivery_mode': 2,
-    headers: headers,
-  }
-
-  channel.publish(exchange, routingKey, Buffer.from(data));
-  console.log(" [x] Sent %s", data);
-}
+});
 
 process.on('exit', (code) => {
   channel.close();
   console.log(`Closing rabbitmq channel`);
 });
+
+// {
+//   expiration: timestamp,
+//   'delivery_mode': 2,
+//   headers: headers,
+// }
